1a524f81b7bb4133c3db5d351a077eff952c895f
KeithTheProisapro <82440499+KeithTheProisapro@users.noreply.github.com>
2021-05-09 10:11:35 +0800

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,10 @@
+number=404
+console.log(number)
+function displayyy() {
+	document.getElementById('error').innerHTML='Spam click by Keith(click when ur mad)(this is a useless tool)'
+}
+counter=0
+function lol(){
+	document.getElementById('count').innerHTML=counter
+	counter++
+}
